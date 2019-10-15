7f28f472195afa332dbf236edee9c6914e7b44f6
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2019-10-15 22:31:40 +0200

    vx

--- sketch.js
+++ sketch.js
@@ -1,25 +1,28 @@
-var tirolermusic;
+var tirolerMusic;
 
 function preload(){
 
-   oktoberfest_img = loadImage("./images/oktoberfest.jpg");
-   tiroler = loadImage("./images/tiroler-normal.png");
-   beer = loadImage("./images/la-chouffe.png");
+   oktoberfest_img = loadImage("./assets/oktoberfest.jpg");
+   tiroler = loadImage("./assets/tiroler-normal.png");
+   beer = loadImage("./assets/la-chouffe.png");
 
-   tirolermusic = loadSound("./assets/tiroler-music.mp3");
+   tirolerMusic = loadSound("./assets/tiroler-music.mp3");
 
   // put preload code here
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  frameRate(10);
+
+   tirolerMusic = createAudio("./assets/tiroler-music.mp3");
+  tirolerMusic.play();
   // put setup code here
 }
 
 function draw() {
 
   new backgroundImage(oktoberfest_img);
+
 //  oktoberfest_img.filter("invert");
 
 //table
@@ -34,6 +37,8 @@ rect(0,windowHeight-100,windowWidth,100);
   //beer
   image( beer,windowWidth/4,windowHeight-beer.height/14-20,beer.width/7,beer.height/7);
   // put drawing code here
+
+
 
 
 
