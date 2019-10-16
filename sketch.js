e042b156a0c012584fa24e3338ed06e503f258b6
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2019-10-16 09:28:53 +0200

    ds

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var tirolerMusic;
+var trashMusic;
 
 function preload(){
 
@@ -6,7 +7,11 @@ function preload(){
    tiroler = loadImage("./assets/tiroler-normal.png");
    beer = loadImage("./assets/la-chouffe.png");
 
+   tirolerD = loadImage("./assets/tiroler.png");
+   beerE = loadImage("./assets/empty-beer.png");
+
    tirolerMusic = loadSound("./assets/tiroler-music.mp3");
+   trashMusic = loadSound("./assets/party-music.mp3");
 
   // put preload code here
 }
@@ -14,28 +19,65 @@ function preload(){
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
-   tirolerMusic = createAudio("./assets/tiroler-music.mp3");
-  tirolerMusic.play();
+
+
   // put setup code here
 }
 
 function draw() {
 
   new backgroundImage(oktoberfest_img);
+  //table
+  noStroke();
+  fill('#8b5e3c');
+  rect(0,windowHeight-100,windowWidth,100);
+   imageMode(CENTER);
+
+   //text
+   fill('white');
+   textAlign(CENTER);
+   textSize(30);
+   text('Drink the beer', windowWidth/2,windowHeight-50);
+
+  if(mouseY>(windowHeight-beerE.height/6-20) && mouseY<(windowHeight-20) &&
+      mouseX>(windowWidth/4-beerE.width/14) && mouseX<(windowWidth/4+beerE.width/14) ){
+
+   //tiroler
+   image(tirolerD,windowWidth/2,windowHeight-tirolerD.height/16-100,tirolerD.width/8,tirolerD.height/8);
+
+   //beer
+   image( beerE,windowWidth/4,windowHeight-beerE.height/14-20,beerE.width/7,beerE.height/7);
+
+   oktoberfest_img.filter("invert");
+
+   if (trashMusic.isPlaying() == false){
+     tirolerMusic.stop();
+     trashMusic.play();
+
+   }
+ }
+   else {
+
+     //tiroler
+     image(tiroler,windowWidth/2,windowHeight-tiroler.height/16-100,tiroler.width/8,tiroler.height/8);
+
+     //beer
+     image( beer,windowWidth/4,windowHeight-beer.height/14-20,beer.width/7,beer.height/7);
+
+     if (tirolerMusic.isPlaying() == false){
+       trashMusic.stop();
+       tirolerMusic.play();
+     }
+
+   }
+//  tirolerMusic = createAudio("./assets/tiroler-music.mp3");
+
+
 
 //  oktoberfest_img.filter("invert");
 
-//table
-noStroke();
-fill('#8b5e3c');
-rect(0,windowHeight-100,windowWidth,100);
 
-  //tiroler
-  imageMode(CENTER);
-  image(tiroler,windowWidth/2,windowHeight-tiroler.height/16-100,tiroler.width/8,tiroler.height/8);
 
-  //beer
-  image( beer,windowWidth/4,windowHeight-beer.height/14-20,beer.width/7,beer.height/7);
   // put drawing code here
 
 
